302bf2d244f1823834749d46528716ef0b23b104
Ryan Irelan <ryan@mijingo.com>
2019-03-23 23:10:09 -0500

    initial functionality updates
    - added ability to un-enroll
    - added method for checking if pathway is complete
    - other misc adjustments

--- src/variables/LearningPathwaysVariable.php
+++ src/variables/LearningPathwaysVariable.php
@@ -31,44 +31,43 @@ class LearningPathwaysVariable
     // Public Methods
     // =========================================================================
 
-    /**
-     * Whatever you want to output to a Twig template can go into a Variable method.
-     * You can have as many variable functions as you want.  From any Twig template,
-     * call it like this:
-     *
-     *     {{ craft.learningPathways.exampleVariable }}
-     *
-     * Or, if your variable requires parameters from Twig:
-     *
-     *     {{ craft.learningPathways.exampleVariable(twigValue) }}
-     *
-     * @param null $optional
-     * @return string
-     */
-    public function exampleVariable($optional = null)
-    {
-        $result = "And away we go to the Twig template...";
-        if ($optional) {
-            $result = "I'm feeling optional today...";
-        }
-        return $result;
-    }
 
+    /**
+     * @param $userData
+     * @return bool
+     */
     public function isEnrolled($userData)
     {
         $result = LearningPathways::$plugin->learningPathwaysService->isEnrolled($userData);
         return $result;
     }
-    
+
+    /**
+     * @return bool
+     */
     public function hasEnrollment()
     {
         $result = LearningPathways::$plugin->learningPathwaysService->hasEnrollment();
         return $result;
     }
 
+    /**
+     * @return array
+     */
     public function getUserPathways()
     {
         $result = LearningPathways::$plugin->learningPathwaysService->getUserPathways();
+        return $result;
+    }
+
+    /**
+     * @param $pathwayEntryId
+     * @return bool
+     */
+    public function isPathwayComplete($pathwayEntryId)
+    {
+        $result = LearningPathways::$plugin->learningPathwaysService->isPathwayComplete($pathwayEntryId);
+
         return $result;
     }
 
