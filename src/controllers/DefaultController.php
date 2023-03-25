3d1b455d2b7f0c8d96a943370c94cb336e7293bd
Ryan Irelan <ryan@mijingo.com>
2023-03-25 00:09:15 -0400

    adding types to allow anonymous

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -46,7 +46,7 @@ class DefaultController extends Controller
      *         The actions must be in 'kebab-case'
      * @access protected
      */
-    protected $allowAnonymous = ['index', 'save', 'delete'];
+    protected bool|array|int $allowAnonymous = ['index', 'save', 'delete'];
 
     // Public Methods
     // =========================================================================
